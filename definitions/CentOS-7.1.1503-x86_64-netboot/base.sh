320d1b985d108fb51e037268d30f2814023d8273
Roman Gorodeckij <holms@holms.lt>
2015-05-11 02:45:26 +0300

    Close #2: EPEL url is incorrect

--- definitions/CentOS-7.1.1503-x86_64-netboot/base.sh
+++ definitions/CentOS-7.1.1503-x86_64-netboot/base.sh
@@ -5,7 +5,7 @@ sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 cat > /etc/yum.repos.d/epel.repo << EOM
 [epel]
 name=epel
-baseurl=http://download.fedoraproject.org/pub/epel/beta/7/\$basearch
+baseurl=http://dl.fedoraproject.org/pub/epel/7/\$basearch
 enabled=1
 gpgcheck=0
 EOM
